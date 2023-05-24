a738bcab3a3e8749572af5048ada2b731f942932
Rafael Garcia <rafael.santos.garcia@hotmail.com>
2023-05-24 00:02:56 -0300

    Primeiro commit

---
+++ src/Main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Main;
+
+/**
+ * @author lucas
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Oi");
+	}
+
+}
